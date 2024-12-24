01b6ccd5c1f12dc0ac3a2e0be7d5b75b009d6209
Soham Joshi <soham0809>
2024-12-24 15:39:14 +0530

    Corrected links once again

--- app.py
+++ app.py
@@ -97,3 +97,14 @@ if __name__ == "__main__":
             )
         else:
             mlflow.sklearn.log_model(lr, "model")
+
+
+import dagshub
+dagshub.init(repo_owner='soham0809',
+             repo_name='mlflowexperiments',
+             mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
